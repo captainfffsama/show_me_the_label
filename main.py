5dae33ab676f6b532c5bead6a7352157239040b6
captainfffsama <tuanzhangsama@outlook.com>
2021-09-09 13:38:23 +0800

    初版

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ import os
 import dash
 from dash import dcc, html
 import plotly.express as px
-import pandas as pd
 from dash.dependencies import Output, Input, State
+import dash_daq as daq
 
 import utils
 
@@ -24,30 +24,61 @@ app.config.suppress_callback_exceptions = True
 app.layout = html.Div(children=[
     dcc.Store(id='all_img_path_list'),
     dcc.Store(id='current_img_path'),
+    html.H1(children='Show Me The Label!',style={
+    'textAlign':'center',
+    }),
+    daq.BooleanSwitch(id="show_introduction_switch", on=True, label="显示说明"),
+    html.Div(id="introduction_show",
+             children=dcc.Markdown('''
+        本工具是用来显示图片的标注,默认会读入图片同目录下的标注,若存在`xml`文件标注将进行显示,若不存在则仅仅显示图片.
 
-    html.H1(children='Show Me The Label!'),
+        输入支持文件夹路径和图片路径,文件夹会递归查找该文件夹下所有图片,当图片很多时加载速度较慢.
+
+        拖动滑条可以快速翻动图片,按按钮可以精细翻动图片.若需要定位显示已经读取的文件列表中特定图片.可以使用搜索下拉框("即显示文件名的地方.")
+
+        目前不支持键盘快捷键,因为 Dash 不支持,而且这东西坑超级多.
+        '''),
+             hidden=False,style={
+    'textAlign':'center',
+    }),
     html.Div(id="w_start_layout",
              children=[
-                 html.H6("请输入文件夹或者图片路径,按 Enter 确认"),
+                 # html.H6("请输入文件夹或者图片路径"),
                  dcc.Input(id="w_dir_input",
                            type="text",
                            placeholder="请输入文件夹所在路径",
                            value="",
                            persistence_type="session",
-                           debounce=True),
-                 html.Div(id="w_show_input_dir")
-             ]),
+                           debounce=False),
+                 html.Div(id="w_show_input_dir"),
+             ],style={
+    'textAlign':'center',
+    }),
     html.Div(children=[
-        html.Div(id="w_imgs_path_slider_layout", hidden=True),
+        dcc.Loading(html.Div(id="w_imgs_path_slider_layout", hidden=True),
+                    type='circle'),
         html.Div(id="w_btn_layout", hidden=True),
-        html.Div(
+        html.H3(
             id="w_show_current_img_path_div",
             children="",
         ),
-        html.Div(id="show_img_layout",children=dcc.Graph(id="show_img_graph"), hidden=False),
-    ]),
+        html.Div(id="show_img_layout",
+                 children=dcc.Loading(dcc.Graph(id="show_img_graph"),
+                                      type='circle'),
+                 hidden=True),
+    ],style={
+    'textAlign':'center',
+    }),
     html.Div(id="test_w"),
 ])
+
+
+@app.callback(
+    output=Output("introduction_show", "hidden"),
+    inputs=Input("show_introduction_switch", "on"),
+)
+def update_introduction_show(show_flag):
+    return not show_flag
 
 
 @app.callback(
@@ -93,16 +124,17 @@ def generate_result_show_widget(all_img_path_list: list):
         hidden = False
         slider_layout = dcc.Slider(id="imgs_path_slider",
                                    min=0,
-                                   max=len(all_img_path_list)-1,
+                                   max=len(all_img_path_list) - 1,
                                    step=1,
                                    value=0)
         btn_layout = [
             dcc.Dropdown(id="files_dropdown",
                          options=[{
-                             'label':os.path.basename(path),
-                             'value':idx
+                             'label': os.path.basename(path),
+                             'value': idx
                          } for idx, path in enumerate(all_img_path_list)],
-                         value=0,clearable=False),
+                         value=0,
+                         clearable=False),
             html.Button(children="上一张", id="w_reduce1_bt", accessKey='d'),
             html.Button(children="下一张", id="w_add1_bt", accessKey='a')
         ]
@@ -111,44 +143,47 @@ def generate_result_show_widget(all_img_path_list: list):
 
 
 @app.callback(
-    output=[Output("imgs_path_slider","value"),
-        Output("files_dropdown","value"),
-        Output("current_img_path","data")
+    output=[
+        Output("imgs_path_slider", "value"),
+        Output("files_dropdown", "value"),
+        Output("current_img_path", "data")
     ],
-    inputs=dict(
-        btn_r=Input("w_reduce1_bt", "n_clicks"),
-        btn_a=Input("w_add1_bt", "n_clicks"),
-        slider_value=Input("imgs_path_slider","value"),
-        files_dropdown_value=Input("files_dropdown","value"),
-        all_img_path=State("all_img_path_list", "data")
-    ),
+    inputs=dict(btn_r=Input("w_reduce1_bt", "n_clicks"),
+                btn_a=Input("w_add1_bt", "n_clicks"),
+                slider_value=Input("imgs_path_slider", "value"),
+                files_dropdown_value=Input("files_dropdown", "value"),
+                all_img_path=State("all_img_path_list", "data")),
     prevent_initial_call=True,
 )
-def connet_bt2slider(btn_r, btn_a,slider_value,files_dropdown_value,all_img_path):
+def connet_bt2slider(btn_r, btn_a, slider_value, files_dropdown_value,
+                     all_img_path):
     if "w_reduce1_bt" == utils.get_callback_id():
-        slider_value -=1
+        slider_value -= 1
     if "w_add1_bt" == utils.get_callback_id():
-        slider_value +=1
+        slider_value += 1
 
     if "files_dropdown" == utils.get_callback_id():
-        slider_value=files_dropdown_value
-    slider_value=max(0,slider_value)
-    slider_value=min(len(all_img_path)-1,slider_value)
-    files_dropdown_value=slider_value
+        slider_value = files_dropdown_value
+    slider_value = max(0, slider_value)
+    slider_value = min(len(all_img_path) - 1, slider_value)
+    files_dropdown_value = slider_value
 
-    return slider_value,files_dropdown_value,all_img_path[slider_value]
+    return slider_value, files_dropdown_value, all_img_path[slider_value]
+
 
 @app.callback(
-    output=[Output("show_img_layout","hidden"),
-            Output("show_img_graph","figure")],
-    inputs=[Input("current_img_path","data"),],
+    output=[
+        Output("show_img_layout", "hidden"),
+        Output("show_img_graph", "figure")
+    ],
+    inputs=[
+        Input("current_img_path", "data"),
+    ],
     prevent_initial_call=True,
-
 )
-def show_img(current_img_path,):
-    fig=utils.show_img(current_img_path)
-    return False,fig
-
+def show_img(current_img_path, ):
+    fig = utils.show_img(current_img_path)
+    return False, fig
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -16,17 +16,19 @@ import plotly.graph_objects as go
 
 
 @lru_cache(None)
-def get_all_file_path(file_dir:str,filter_:tuple=('.jpg',)) -> list:
+def get_all_file_path(file_dir: str, filter_: tuple = ('.jpg', )) -> list:
     #遍历文件夹下所有的file
     return [os.path.join(maindir,filename) for maindir,_,file_name_list in os.walk(file_dir) \
             for filename in file_name_list \
             if os.path.splitext(filename)[1] in filter_ ]
+
 
 def get_callback_id(no_attr=True):
     if no_attr:
         return dash.callback_context.triggered[0]['prop_id'].split('.')[0]
     else:
         return dash.callback_context.triggered[0]['prop_id']
+
 
 def parse_rec(filename) -> list:
     r"""
@@ -41,78 +43,118 @@ def parse_rec(filename) -> list:
         obj_struct['truncated'] = int(obj.find('truncated').text)
         obj_struct['difficult'] = int(obj.find('difficult').text)
         bbox = obj.find('bndbox')
-        obj_struct['bbox'] = [int(bbox.find('xmin').text),
-                              int(bbox.find('ymin').text),
-                              int(bbox.find('xmax').text),
-                              int(bbox.find('ymax').text)]
+        obj_struct['bbox'] = [
+            int(bbox.find('xmin').text),
+            int(bbox.find('ymin').text),
+            int(bbox.find('xmax').text),
+            int(bbox.find('ymax').text)
+        ]
         objects.append(obj_struct)
 
     return objects
+
 
 def pil_to_fig(im, showlegend=False, title=None):
     img_width, img_height = im.size
     fig = go.Figure()
     # This trace is added to help the autoresize logic work.
-    fig.add_trace(go.Scatter(
-        x=[img_width * 0.05, img_width * 0.95],
-        y=[img_height * 0.95, img_height * 0.05],
-        showlegend=False, mode="markers", marker_opacity=0,
-        hoverinfo="none", legendgroup='Image'))
+    fig.add_trace(
+        go.Scatter(x=[img_width * 0.05, img_width * 0.95],
+                   y=[img_height * 0.95, img_height * 0.05],
+                   showlegend=False,
+                   mode="markers",
+                   marker_opacity=0,
+                   hoverinfo="none",
+                   legendgroup='Image'))
 
-    fig.add_layout_image(dict(
-        source=im,  opacity=1, layer="below",
-        x=0, y=0, xref="x", yref="y", sizex=img_width, sizey=img_height,))
+    fig.add_layout_image(
+        dict(
+            source=im,
+            opacity=1,
+            layer="below",
+            x=0,
+            y=0,
+            xref="x",
+            yref="y",
+            sizex=img_width,
+            sizey=img_height,
+        ))
 
     # Adapt axes to the right width and height, lock aspect ratio
-    fig.update_xaxes(
-        showgrid=False, visible=False, constrain="domain", range=[0, img_width])
+    fig.update_xaxes(showgrid=False,
+                     visible=False,
+                     constrain="domain",
+                     range=[0, img_width])
 
-    fig.update_yaxes(
-        showgrid=False, visible=False,
-        scaleanchor="x", scaleratio=1,
-        range=[img_height, 0])
+    fig.update_yaxes(showgrid=False,
+                     visible=False,
+                     scaleanchor="x",
+                     scaleratio=1,
+                     range=[img_height, 0])
 
-    fig.update_layout(title=title, showlegend=showlegend,width=1920,height=1080)
+    fig.update_layout(title=title,
+                      showlegend=showlegend,
+                      width=1920,
+                      height=1080)
 
     return fig
 
-def add_bbox(fig, x0, y0, x1, y1,
-             showlegend=True, name=None, color=None,
-             opacity=0.5, group=None, text=None):
-    fig.add_trace(go.Scatter(
-        x=[x0, x1, x1, x0, x0],
-        y=[y0, y0, y1, y1, y0],
-        mode="lines",
-        fill="toself",
-        opacity=opacity,
-        marker_color=color,
-        hoveron="fills",
-        name=name,
-        hoverlabel_namelength=0,
-        text=text,
-        legendgroup=group,
-        showlegend=showlegend,
-    ))
+
+def add_bbox(fig,
+             x0,
+             y0,
+             x1,
+             y1,
+             showlegend=True,
+             name=None,
+             color=None,
+             opacity=0.5,
+             group=None,
+             text=None,font_size=10,):
+    fig.add_shape(type="rect",
+                  x0=x0,
+                  y0=y0,
+                  x1=x1,
+                  y1=y1,
+                  line=dict(color=color))
+    text_size=max(font_size,min((x1-x0),(y1-y0))//10)
+    fig.add_annotation(x=x0,
+                       y=y1,
+                       text=text,
+                       font=dict(color="yellow",size=text_size),
+                       xanchor="left",
+                       yanchor="bottom",
+                       align="left",
+                       showarrow=False)
+
 
 @lru_cache(None)
 def show_img(img_path):
-    img=Image.open(img_path)
+    img = Image.open(img_path)
 
-    fig=pil_to_fig(img)
+    fig = pil_to_fig(img)
 
-
-    xml_path=os.path.splitext(img_path)[0]+".xml"
+    xml_path = os.path.splitext(img_path)[0] + ".xml"
+    font_size=max(1,min(img.size)//30)
 
     if os.path.exists(xml_path):
-        ann_obj=parse_rec(xml_path)
+        ann_obj = parse_rec(xml_path)
         for obj in ann_obj:
-            x0, y0, x1,y1=obj["bbox"]
-            label=obj["name"]
+            x0, y0, x1, y1 = obj["bbox"]
+            label = obj["name"]
             add_bbox(
-                fig, x0, y0, x1, y1,
-                opacity=0.7, group=label, name=label, color="green",
-                showlegend=True, text=label,
+                fig,
+                x0,
+                y0,
+                x1,
+                y1,
+                opacity=0.7,
+                group=label,
+                name=label,
+                color="green",
+                showlegend=True,
+                text=label,
+                font_size=font_size,
             )
-
 
     return fig
